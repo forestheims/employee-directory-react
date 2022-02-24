1fbb4e249c36f3eb013fa394ca2906de6a23c845
forestheims <forestheims@gmail.com>
2022-02-23 16:09:56 -0800

    authContext file and functions created

---
+++ src/context/authContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export function authProvider({ children }) {
+  const [user, setUser] = useState('');
+
+  const contextValue = { user, setUser };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('Error: useUser needs to be called within a UserProvider');
+  }
+  return context;
+}
