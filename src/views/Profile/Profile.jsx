43c002f0b66785802b841f44fec227aa27625e42
forestheims <forestheims@gmail.com>
2022-02-25 12:19:47 -0800

    Profile view updated to Direct user to the Add Profile page if no profile exists.

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
 import { useProfile } from '../../context/profilesContext';
 
 export default function Profile() {
   const { profile } = useProfile();
-  // const [componentProfile, setComponentProfile] = useState({});
-
-  // while (profile.email !== '') {
-  //   setComponentProfile(profile);
-  // }
-  useEffect(() => {}, []);
 
   return (
     <>
+      {profile.name ? (
+        <></>
+      ) : (
+        <>
+          <h1>It looks like you have not made a profile yet.</h1>
+          <h2>
+            Follow the 'Add Profile' link at the top of the page to create your
+            profile
+          </h2>
+        </>
+      )}
       <p>Employee Name: {profile.name}</p>
       <p>Email: {profile.email}</p>
       <p>Birthday: {profile.birthday}</p>
